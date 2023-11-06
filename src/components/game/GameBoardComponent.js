29882ebe65b68ae9102b5bafb5197d8f1b8fa2e5
tc <ychernovskaya@gmail.com>
2023-11-07 00:46:55 +0700

    add snake

--- src/components/game/GameBoardComponent.js
+++ src/components/game/GameBoardComponent.js
@@ -1,18 +1,40 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import ConfigService from "../../api/ConfigService";
 
 function GameBoardComponent({n, m}) {
+    const [coordsFromResponse, setCoordsFromResponse] = useState([]);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            ConfigService.getGameState()
+                .then(response => {
+                    const {snakeList} = response.data;
+                    if (Array.isArray(snakeList) && snakeList.length > 0) {
+                        console.log(snakeList);
+                        const apiCoords = snakeList.map(snake => snake.coordList).flat();
+                        console.log(apiCoords);
+                        setCoordsFromResponse(apiCoords);
+                    }
+                })
+                .catch(error => {
+                    console.error('Ошибка при получении данных:', error);
+                });
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
     const height = 80 + 'vh';
-
     const squareSize = 80 / m;
-
-    const width = n * squareSize + 'vh'
+    const width = n * squareSize + 'vh';
 
     const squares = [];
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             const uniqueId = i * n + j;
+            const isColored = coordsFromResponse.some(coord => coord.x === j && coord.y === i);
             squares.push(
                 <div
                     key={uniqueId}
@@ -23,20 +45,20 @@ function GameBoardComponent({n, m}) {
                         boxSizing: 'border-box',
                         paddingBottom: squareSize,
                         display: 'inline-block',
-                        margin:0,
-                        padding:0
+                        margin: 0,
+                        padding: 0,
+                        backgroundColor: isColored ? '#FF5733' : 'white',
                     }}
-                >
-
-                </div>
+                />
             );
         }
     }
+
     return (
-        <div style={{ width, height }} className='board'>
+        <div style={{width, height}} className='board'>
             {squares}
         </div>
     );
 }
 
-export default GameBoardComponent
+export default GameBoardComponent;
